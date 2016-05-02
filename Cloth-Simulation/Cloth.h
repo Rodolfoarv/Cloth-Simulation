022422e7e7f15d875e7bc205cb9df0c098856058
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-05-01 21:47:18 -0500

    Added initial implementation of the header file for Cloth

---
+++ Cloth-Simulation/Cloth.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include "Particle.h"
+#include "Constraint.h"
+class Cloth
+{
+private:
+	int num_particles_width; // number of particles in "width" direction
+	int num_particles_height; // number of particles in "height" direction
+	std::vector<Particle> particles;
+	std::vector<Constraint> constraints;
+
+
+
+public:
+	Cloth();
+	~Cloth();
+	Cloth(float width, float height, int num_particles_width, int num_particles_height);
+	void addForce(const Vec3 direction);
+
+};
+
+
+
