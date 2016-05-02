6bd4d497eeee52b9c493901b70618a3811614e41
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-05-01 23:21:25 -0500

    Added main class and init method

---
+++ Cloth-Simulation/Main.cpp
@@ -0,0 +1,32 @@
+#include "Main.h"
+#include <GL/glut.h> 
+#include <GL/gl.h>
+
+void init(GLvoid)
+{
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.2f, 0.2f, 0.4f, 0.5f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_COLOR_MATERIAL);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	GLfloat lightPos[4] = { -1.0, 1.0, 0.5, 0.0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, (GLfloat *)&lightPos);
+
+	glEnable(GL_LIGHT1);
+
+	GLfloat lightAmbient1[4] = { 0.0, 0.0, 0.0, 0.0 };
+	GLfloat lightPos1[4] = { 1.0, 0.0, -0.2, 0.0 };
+	GLfloat lightDiffuse1[4] = { 0.5, 0.5, 0.3, 0.0 };
+
+	glLightfv(GL_LIGHT1, GL_POSITION, (GLfloat *)&lightPos1);
+	glLightfv(GL_LIGHT1, GL_AMBIENT, (GLfloat *)&lightAmbient1);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, (GLfloat *)&lightDiffuse1);
+
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+}
+
