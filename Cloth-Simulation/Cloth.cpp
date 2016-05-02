5a41a5f7d5998962579d995fade98f5316a55e7e
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-05-01 21:53:12 -0500

    Added implementation of getParticle

--- Cloth-Simulation/Cloth.cpp
+++ Cloth-Simulation/Cloth.cpp
@@ -10,6 +10,9 @@ Cloth::~Cloth()
 {
 }
 
+Particle* Cloth::getParticle(int x, int y) { return &particles[y*num_particles_width + x]; }
+
+
 Cloth::Cloth(float width, float height, int num_particles_width, int num_particles_height)
 	: num_particles_width(num_particles_width), num_particles_height(num_particles_height)
 {
@@ -23,4 +26,7 @@ Cloth::Cloth(float width, float height, int num_particles_width, int num_particl
 		getParticle(0 + i, 0)->offsetPos(Vec3(-0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
 		getParticle(num_particles_width - 1 - i, 0)->makeUnmovable();
 	}
+
 }
+
+
