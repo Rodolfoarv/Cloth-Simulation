dcb9d408c118f40678395097af5c8e299003a168
Rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-05-01 21:58:18 -0500

    Modified Cloth constructor to create particles depending on width and height

--- Cloth-Simulation/Cloth.cpp
+++ Cloth-Simulation/Cloth.cpp
@@ -17,13 +17,26 @@ Cloth::Cloth(float width, float height, int num_particles_width, int num_particl
 	: num_particles_width(num_particles_width), num_particles_height(num_particles_height)
 {
 	particles.resize(num_particles_width*num_particles_height); 
+
+	// Creates the particle from 0,0,0 to the width and height being specified
+	for (int x = 0; x<num_particles_width; x++)
+	{
+		for (int y = 0; y<num_particles_height; y++)
+		{
+			Vec3 pos = Vec3(width * (x / (float)num_particles_width),
+				-height * (y / (float)num_particles_height),
+				0);
+			particles[y*num_particles_width + x] = Particle(pos); // insert particle in column x at y'th row
+		}
+	}
+
 	// making the upper left most three and right most three particles unmovable
 	for (int i = 0; i<3; i++)
 	{
-		getParticle(0 + i, 0)->offsetPos(Vec3(0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
+		//In order to make the particle to hang more on the center
+		getParticle(0 + i, 0)->offsetPos(Vec3(0.5, 0.0, 0.0)); 
 		getParticle(0 + i, 0)->makeUnmovable();
-
-		getParticle(0 + i, 0)->offsetPos(Vec3(-0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
+		getParticle(0 + i, 0)->offsetPos(Vec3(-0.5, 0.0, 0.0)); 
 		getParticle(num_particles_width - 1 - i, 0)->makeUnmovable();
 	}
 
